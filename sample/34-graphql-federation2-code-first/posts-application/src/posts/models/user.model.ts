76afa869880f33631d803ed04f04a638738d8363
Ben Heidemann <56122437+bcheidemann@users.noreply.github.com>
2022-11-07 18:17:50 +0000

    initial implementation

---
+++ sample/34-graphql-federation2-code-first/posts-application/src/posts/models/user.model.ts
@@ -0,0 +1,15 @@
+import { Directive, Field, ID, ObjectType } from '@nestjs/graphql';
+import { Post } from './post.model';
+
+@ObjectType()
+// @Directive('@extends')
+// @Directive('@external')
+@Directive('@key(fields: "id")')
+export class User {
+  @Field((type) => ID)
+  // @Directive('@external')
+  id: number;
+
+  @Field((type) => [Post])
+  posts?: Post[];
+}
